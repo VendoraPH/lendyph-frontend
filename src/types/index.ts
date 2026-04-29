57ae0e07313fd2359f55707c6e7aedad1c9eea41
Augustin Maputol <augustinmaputol@vendoraph.com>
2026-04-30 00:16:35 +0800

    fix(types): remove ValidId re-export that was missing from borrower.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type { User, UserStatus, UserBranch } from "./user";
 export type { Loan, LoanStatus, InterestType, LoanSchedule, LoanProduct } from "./loan";
-export type { Borrower, BorrowerLedgerEntry, CivilStatus, Gender, BorrowerStatus, ValidIdType, ValidId, EmploymentType } from "./borrower";
+export type { Borrower, BorrowerLedgerEntry, CivilStatus, Gender, BorrowerStatus, ValidIdType, EmploymentType } from "./borrower";
 export type { CoMaker, CoMakerRelationship } from "./co-maker";
 export type { Payment } from "./payment";
 export type { Collection } from "./collection";
